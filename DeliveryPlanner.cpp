ac260581df9604651acd0d5d376342d6595b5403
Alex Chen <32936005+achen2289@users.noreply.github.com>
2020-03-09 22:57:56 -0700

    delivery planner re-code in progress

--- DeliveryPlanner.cpp
+++ DeliveryPlanner.cpp
@@ -16,8 +16,8 @@ public:
 private:
     DeliveryOptimizer* dop;
     PointToPointRouter* ptp;
-    bool routeToCommand(list<StreetSegment>& route, vector<DeliveryCommand>& commands);
-    string directionString(double angle);
+    bool routeToCommand(list<StreetSegment>& route, vector<DeliveryCommand>& commands) const;
+    string directionString(double angle) const;
 };
 
 DeliveryPlannerImpl::DeliveryPlannerImpl(const StreetMap* sm)
@@ -63,10 +63,7 @@ DeliveryResult DeliveryPlannerImpl::generateDeliveryPlan(
         {
             prev = reorderedDel[i].location;
             totalDistanceTravelled += tempDist;
-            for (auto itr = route.begin(); itr != route.end(); itr++)
-            {
-                
-            }
+            routeToCommand(route, commands);
         }
         else
             return dr;
@@ -74,95 +71,98 @@ DeliveryResult DeliveryPlannerImpl::generateDeliveryPlan(
     return dr; // dr should contain DELIVERY_SUCESS
 }
 
-bool DeliveryPlannerImpl::routeToCommand(list<StreetSegment>& route, vector<DeliveryCommand>& commands)
+bool DeliveryPlannerImpl::routeToCommand(list<StreetSegment>& route, vector<DeliveryCommand>& commands) const
 {
-    auto itr = route.begin(); // first street segment
-    string currStr = (*itr).name; // current street name
-    StreetSegment prevSS = *itr; // holds previous SS
-    
-    DeliveryCommand com1; // holds first delivery command
-    com1.initAsProceedCommand(directionString(angleOfLine(*itr)), currStr, distanceEarthMiles((*itr).start, (*itr).end)); // first command put in
-    
-    itr++;
-    
-    // runs and increments distance to travel until different street reached
-    while (itr != route.end() && (*itr).name == currStr)
-    {
-        com1.increaseDistance(distanceEarthMiles((*itr).start, (*itr).end));
-        prevSS = *itr;
-        itr++;
-    }
-    currStr = (*itr).name; // maintains street name
-    commands.push_back(com1);
-     
-    // iterates through SS with 2nd street name until last SS
+    string previous = "";
+    auto itr = route.begin();
     while (itr != route.end())
     {
-        DeliveryCommand com; // holds any delivery command
-        double angleBet = angleBetween2Lines(prevSS, *itr);
-        if (angleBet < 1 || angleBet > 359) // prev always first argument
+        // first street segment must create new proceed command
+        if (itr == route.begin())
         {
-            com.initAsProceedCommand(directionString(angleOfLine(*itr)), currStr, distanceEarthMiles((*itr).start, (*itr).end));
-            prevSS = *itr;
+            DeliveryCommand com;
+            double angle = angleOfLine(*itr);
+            double dist = distanceEarthMiles((*itr).start, (*itr).end);
+            com.initAsProceedCommand(directionString(angle), (*itr).name, dist);
+            previous = (*itr).name; // name of first SS
             itr++;
-            while (itr != route.end() && (*itr).name == currStr)
+            
+            while (itr != route.end() && (*itr).name == previous)
             {
-                com.increaseDistance(distanceEarthMiles((*itr).start, (*itr).end));
-                prevSS = *itr;
+                double dist = distanceEarthMiles((*itr).start, (*itr).end);
+                com.increaseDistance(dist);
+                previous = (*itr).name; // name of SS
                 itr++;
             }
-            currStr = (*itr).name;
-            commands.push_back(com);
-        }
-        else if (angleBet >= 1 && angleBet < 180)
-        {
-            com.initAsTurnCommand("left", currStr);
-            commands.push_back(com);
-            DeliveryCommand com2;
-            com2.initAsProceedCommand(directionString(angleOfLine(*itr)), currStr, distanceEarthMiles((*itr).start, (*itr).end));
-        }
-        else // if (angleBet >= 180 && angleBet <= 359)
-        {
-            com.initAsTurnCommand("right", currStr);
             commands.push_back(com);
         }
         
-        if ((*itr).name == currStr)
+        // commands following first SS command
+        if (itr != route.end() && (*itr).name != previous)
         {
-            while (itr != route.end() && (*itr).name == currStr)
+            DeliveryCommand comm;
+            auto itr2 = itr;
+            itr2--; // iterator to previous SS
+            double angleBet = angleBetween2Lines(*itr2, *itr);
+            if (angleBet < 1 || angleBet > 359)
             {
-                com.increaseDistance(distanceEarthMiles((*itr).start, (*itr).end));
+                double angle = angleOfLine(*itr);
+                double dist = distanceEarthMiles((*itr).start, (*itr).end);
+                comm.initAsProceedCommand(directionString(angle), ((*itr).name), dist);
+                previous = (*itr).name;
+                itr++;
+            }
+            else if (angleBet >= 1 && angleBet < 180)
+            {
+                DeliveryCommand commT;
+                commT.initAsTurnCommand("left", (*itr).name);
+                commands.push_back(commT);
+                previous = (*itr).name;
+                itr++;
+            }
+            else // if (angleBet >= 180 && angleBet <= 359)
+            {
+                DeliveryCommand commT;
+                commT.initAsTurnCommand("right", (*itr).name);
+                commands.push_back(commT);
+                previous = (*itr).name;
+                itr++;
+            }
+            while (itr != route.end() && (*itr).name == previous)
+            {
+                double dist = distanceEarthMiles((*itr).start, (*itr).end);
+                comm.increaseDistance(dist);
+                previous = (*itr).name; // name of SS
                 itr++;
             }
         }
         else
-            itr++;
-        commands.push_back(com);
-        currStr = (*itr).name; // new street name
-//        itr++;
+        {
+            previous = (*itr).name;
+        }
     }
     return true;
 }
 
-string DeliveryPlannerImpl::directionString(double angle)
+string DeliveryPlannerImpl::directionString(double angle) const
 {
     if (angle >= 0 && angle < 22.5)
         return "east";
-    if (angle >= 22.5 && angle < 67.5)
+    else if (angle >= 22.5 && angle < 67.5)
         return "northeast";
-    if (angle >= 67.5 && angle < 112.5)
+    else if (angle >= 67.5 && angle < 112.5)
         return "north";
-    if (angle >= 112.5 && angle < 157.5)
+    else if (angle >= 112.5 && angle < 157.5)
         return "northwest";
-    if (angle >= 157.5 && angle < 202.5)
+    else if (angle >= 157.5 && angle < 202.5)
         return "west";
-    if (angle >= 202.5 && angle < 247.5)
+    else if (angle >= 202.5 && angle < 247.5)
         return "southwest";
-    if (angle >= 247.5 && angle < 292.5)
+    else if (angle >= 247.5 && angle < 292.5)
         return "south";
-    if (angle >= 292.5 && angle < 337.5)
+    else if (angle >= 292.5 && angle < 337.5)
         return "southeast";
-    if (angle >= 337.5)
+    else // (angle >= 337.5)
         return "east";
 }
 
